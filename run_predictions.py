f5d736b9cf8230d1a7731a18466dcdf62f18aa3b
ywwwei <2015212903@bupt.edu.cn>
2020-04-13 00:23:29 -0700

    matched filtering in red channel

--- run_predictions.py
+++ run_predictions.py
@@ -24,26 +24,48 @@ def detect_red_light(I):
     '''
     BEGIN YOUR CODE
     '''
-    
-    '''
-    As an example, here's code that generates between 1 and 5 random boxes
-    of fixed size and returns the results in the proper format.
-    '''
-    
-    box_height = 8
-    box_width = 6
-    
-    num_boxes = np.random.randint(1,5) 
-    
-    for i in range(num_boxes):
-        (n_rows,n_cols,n_channels) = np.shape(I)
-        
-        tl_row = np.random.randint(n_rows - box_height)
-        tl_col = np.random.randint(n_cols - box_width)
-        br_row = tl_row + box_height
-        br_col = tl_col + box_width
-        
-        bounding_boxes.append([tl_row,tl_col,br_row,br_col]) 
+    data_path = 'data/kernel'
+    kernel = Image.open(os.path.join(data_path,'kernel.jpg'))
+    kernel = np.asarray(kernel)
+    box_height = kernel.shape[0]
+    box_width = kernel.shape[1]
+
+    r_kernel = kernel[:,:,0]
+    r_kernel = r_kernel / np.linalg.norm(r_kernel) # normalize
+    r_I = I[:,:,0]
+
+    threshold = 0.9
+    for row in range(r_I.shape[0]-box_height):
+        for col in range(r_I.shape[1]-box_width):
+            r_I_part = r_I[row:row+box_height,col:col+box_width]
+            r_I_part = r_I_part / np.linalg.norm(r_I_part) # normalize
+            convolution = np.sum(np.multiply(r_kernel,r_I_part))
+            if convolution > threshold:
+                tl_row = row
+                tl_col = col
+                br_row = tl_row + box_height
+                br_col = tl_col + box_width
+                bounding_boxes.append([tl_row,tl_col,br_row,br_col])
+
+    # '''
+    # As an example, here's code that generates between 1 and 5 random boxes
+    # of fixed size and returns the results in the proper format.
+    # '''
+    #
+    # box_height = 8
+    # box_width = 6
+    #
+    # num_boxes = np.random.randint(1,5)
+    #
+    # for i in range(num_boxes):
+    #     (n_rows,n_cols,n_channels) = np.shape(I)
+    #
+    #     tl_row = np.random.randint(n_rows - box_height)
+    #     tl_col = np.random.randint(n_cols - box_width)
+    #     br_row = tl_row + box_height
+    #     br_col = tl_col + box_width
+    #
+    #     bounding_boxes.append([tl_row,tl_col,br_row,br_col])
     
     '''
     END YOUR CODE
@@ -55,10 +77,10 @@ def detect_red_light(I):
     return bounding_boxes
 
 # set the path to the downloaded data: 
-data_path = '../data/RedLights2011_Medium'
+data_path = 'data/RedLights2011_Medium'
 
 # set a path for saving predictions: 
-preds_path = '../data/hw01_preds' 
+preds_path = 'data/hw01_preds'
 os.makedirs(preds_path,exist_ok=True) # create directory if needed 
 
 # get sorted list of files: 
@@ -81,3 +103,7 @@ for i in range(len(file_names)):
 # save preds (overwrites any previous predictions!)
 with open(os.path.join(preds_path,'preds.json'),'w') as f:
     json.dump(preds,f)
+
+
+# with open("data_file.json", "r") as read_file:
+#     data = json.load(read_file)
