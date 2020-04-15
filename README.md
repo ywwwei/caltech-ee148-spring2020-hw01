dd20bf0a9e14a16cb3ff6bcb1a837c03b4412003
ywwwei <2015212903@bupt.edu.cn>
2020-04-14 21:33:37 -0700

    matched filtering in red channel

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # caltech-ee148-spring2020-hw01
 
-The link to the instructions is available on Piazza.
+Three detect_red_light_* functions in run_predictions.py
+detect_red_light_simple: pick one kernel for all input images
+detect_red_light_random: pick one red light image randomly from /data/kernel/ as kernel for each imput image
+detect_red_light_average: use the average red light image as kernel for all input images
+
+Code to generate the visualization resualts is in hw1_visualization.ipynb
+
--- run_predictions.py
+++ run_predictions.py
@@ -3,7 +3,7 @@ import numpy as np
 import json
 from PIL import Image
 
-def detect_red_light(I):
+def detect_red_light_simple(I):
     '''
     This function takes a numpy array <I> and returns a list <bounding_boxes>.
     The list <bounding_boxes> should have one element for each red light in the 
@@ -25,7 +25,7 @@ def detect_red_light(I):
     BEGIN YOUR CODE
     '''
     data_path = 'data/kernel'
-    kernel = Image.open(os.path.join(data_path,'kernel.jpg'))
+    kernel = Image.open(os.path.join(data_path,'kernel1.jpg'))
     kernel = np.asarray(kernel)
     box_height = kernel.shape[0]
     box_width = kernel.shape[1]
@@ -34,13 +34,14 @@ def detect_red_light(I):
     r_kernel = r_kernel / np.linalg.norm(r_kernel) # normalize
     r_I = I[:,:,0]
 
-    threshold = 0.9
+    threshold = 0.96
     for row in range(r_I.shape[0]-box_height):
         for col in range(r_I.shape[1]-box_width):
             r_I_part = r_I[row:row+box_height,col:col+box_width]
             r_I_part = r_I_part / np.linalg.norm(r_I_part) # normalize
             convolution = np.sum(np.multiply(r_kernel,r_I_part))
-            if convolution > threshold:
+            # to avoid overlapped boxes
+            if convolution > threshold and (not bounding_boxes or (row > tl_row + 5 and col > tl_col+5)):
                 tl_row = row
                 tl_col = col
                 br_row = tl_row + box_height
@@ -76,6 +77,116 @@ def detect_red_light(I):
     
     return bounding_boxes
 
+
+def detect_red_light_random(I):
+    '''
+    This function takes a numpy array <I> and returns a list <bounding_boxes>.
+    The list <bounding_boxes> should have one element for each red light in the
+    image. Each element of <bounding_boxes> should itself be a list, containing
+    four integers that specify a bounding box: the row and column index of the
+    top left corner and the row and column index of the bottom right corner (in
+    that order). See the code below for an example.
+
+    Note that PIL loads images in RGB order, so:
+    I[:,:,0] is the red channel
+    I[:,:,1] is the green channel
+    I[:,:,2] is the blue channel
+    '''
+
+
+    bounding_boxes = [] # This should be a list of lists, each of length 4. See format example below.
+
+    '''
+    BEGIN YOUR CODE
+    '''
+    data_path = 'data/kernel'
+    idx = np.random.randint(172)
+    kernel = Image.open(os.path.join(data_path,'kernel'+str(idx+1)+'.jpg'))
+    kernel = np.asarray(kernel)
+    box_height = kernel.shape[0]
+    box_width = kernel.shape[1]
+
+    r_kernel = kernel[:,:,0]
+    r_kernel = r_kernel / np.linalg.norm(r_kernel) # normalize
+    r_I = I[:,:,0]
+
+    threshold = 0.9
+    for row in range(r_I.shape[0]-box_height):
+        for col in range(r_I.shape[1]-box_width):
+            r_I_part = r_I[row:row+box_height,col:col+box_width]
+            r_I_part = r_I_part / np.linalg.norm(r_I_part) # normalize
+            convolution = np.sum(np.multiply(r_kernel,r_I_part))
+            # to avoid overlapped boxes
+            if convolution > threshold and (not bounding_boxes or (row > tl_row + 5 and col > tl_col+5)):
+                tl_row = row
+                tl_col = col
+                br_row = tl_row + box_height
+                br_col = tl_col + box_width
+                bounding_boxes.append([tl_row,tl_col,br_row,br_col])
+
+    '''
+    END YOUR CODE
+    '''
+
+    for i in range(len(bounding_boxes)):
+        assert len(bounding_boxes[i]) == 4
+
+    return bounding_boxes
+
+def detect_red_light_average(I):
+    '''
+    This function takes a numpy array <I> and returns a list <bounding_boxes>.
+    The list <bounding_boxes> should have one element for each red light in the
+    image. Each element of <bounding_boxes> should itself be a list, containing
+    four integers that specify a bounding box: the row and column index of the
+    top left corner and the row and column index of the bottom right corner (in
+    that order). See the code below for an example.
+
+    Note that PIL loads images in RGB order, so:
+    I[:,:,0] is the red channel
+    I[:,:,1] is the green channel
+    I[:,:,2] is the blue channel
+    '''
+
+
+    bounding_boxes = [] # This should be a list of lists, each of length 4. See format example below.
+
+    '''
+    BEGIN YOUR CODE
+    '''
+    data_path = 'data/kernel_resized'
+    kernel = Image.open(os.path.join(data_path,'kernel_ave.jpg'))
+    kernel = np.asarray(kernel)
+    box_height = kernel.shape[0]
+    box_width = kernel.shape[1]
+
+    r_kernel = kernel[:,:,0]
+    r_kernel = r_kernel / np.linalg.norm(r_kernel) # normalize
+    r_I = I[:,:,0]
+
+    threshold = 0.96
+    for row in range(r_I.shape[0]-box_height):
+        for col in range(r_I.shape[1]-box_width):
+            r_I_part = r_I[row:row+box_height,col:col+box_width]
+            r_I_part = r_I_part / np.linalg.norm(r_I_part) # normalize
+            convolution = np.sum(np.multiply(r_kernel,r_I_part))
+            # to avoid overlapped boxes
+            if convolution > threshold and (not bounding_boxes or (row > tl_row + 5 and col > tl_col+5)):
+                tl_row = row
+                tl_col = col
+                br_row = tl_row + box_height
+                br_col = tl_col + box_width
+                bounding_boxes.append([tl_row,tl_col,br_row,br_col])
+
+    '''
+    END YOUR CODE
+    '''
+
+    for i in range(len(bounding_boxes)):
+        assert len(bounding_boxes[i]) == 4
+
+    return bounding_boxes
+
 # set the path to the downloaded data: 
 data_path = 'data/RedLights2011_Medium'
 
@@ -91,6 +202,7 @@ file_names = [f for f in file_names if '.jpg' in f]
 
 preds = {}
 for i in range(len(file_names)):
+    print(i)
     
     # read image using PIL:
     I = Image.open(os.path.join(data_path,file_names[i]))
@@ -98,11 +210,14 @@ for i in range(len(file_names)):
     # convert to numpy array:
     I = np.asarray(I)
     
-    preds[file_names[i]] = detect_red_light(I)
+    preds[file_names[i]] = detect_red_light_average(I)
+    print(preds[file_names[i]])
 
 # save preds (overwrites any previous predictions!)
-with open(os.path.join(preds_path,'preds.json'),'w') as f:
+with open(os.path.join(preds_path,'preds_average.json'),'w') as f:
     json.dump(preds,f)
+
+
 
 
 # with open("data_file.json", "r") as read_file:
